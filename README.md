a0ddb68ad9b475059afaf5f5f4353430ce6b46db
Kyle Chinick <kylechinick@icloud.com>
2022-06-09 11:04:50 -0700

    Add test to turn string into array and test to turn array elements into numbers
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>
    Co-authored-by: Ryan DeFea <ryan.defea@gmail.com>

--- README.md
+++ README.md
@@ -25,3 +25,14 @@ _Author_:
 ## License
 
 ...
+Test: "It will take data a turn it into a string"
+Code: const cardNumber = 1234; cardNumber.toString();
+Expected Output: "1234"
+
+Test: "It will take the string and turn it into an array"
+Code: const cardNumber = '1234'; cardNumber.split();
+Expected Output: ['1','2','3','4']
+
+Test: "It will take the array and turn elements in the array into numbers"
+Code: const cardNumber = ['1','2','3','4']; cardNumber.map(Number);
+Expected Output: [1,2,3,4]
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 console.log('test');
 
 const cardNumberTest = 1234;
+console.log('cardNumberTest: ', cardNumberTest);
 
 function validate(cardNumber) {
-  console.log('cardNumberTest: ', cardNumber);
-  const digitsToString = cardNumber.toString();
-  console.log('digitsToString: ', digitsToString);
-  const digitsToStringSplit = cardNumber.toString().split('');
-  console.log('digitsToStringSplit ', digitsToStringSplit);
-  const digitsToStringSplitMap = cardNumber.toString().split('').map(Number);
-  console.log('digitsToStringSplitMap ', digitsToStringSplitMap);
-  // const digits = cardNumber.toString().split('').map(Number);
-  // console.log(digits);
+  // // TURN DATA INTO STRING
+  // const digitsToString = cardNumber.toString();
+  // console.log('digitsToString: ', digitsToString);
+
+  // // TURN STRING INTO ELEMENTS IN AN ARRAY
+  // const digitsToStringSplit = cardNumber.toString().split('');
+  // console.log('digitsToStringSplit ', digitsToStringSplit);
+
+  // // TURN ELEMENTS IN ARRAY INTO NUMBERS
+  // const digitsToStringSplitMap = cardNumber.toString().split('').map(Number);
+  // console.log('digitsToStringSplitMap ', digitsToStringSplitMap);
+
+  const digits = cardNumber.toString().split('').map(Number);
+  console.log(digits);
 }
 
 validate(cardNumberTest);
-
-// if (x > 10) {
-//   x / 2;
-// }
